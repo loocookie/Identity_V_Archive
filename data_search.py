80f87cd1eafd67221a8a8055322e8f56915cc7d1
loocookie <138028741+loocookie@users.noreply.github.com>
2024-04-11 23:06:21 +0900

    Update data_search.py

--- data_search.py
+++ data_search.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from st_screen_stats import ScreenData
 import pandas as pd
 import numpy as np
 
@@ -7,8 +6,16 @@ import numpy as np
 st.set_page_config(page_title="IVL Archive", layout="wide")
 st.title("IVL Archive")
 
-screenD = ScreenData(setTimeout=0)
-screen_d = screenD.st_screen_data_window_top()
+
+try:
+    from st_screen_stats import ScreenData
+    screenD = ScreenData(setTimeout=0)
+    screen_d = screenD.st_screen_data_window_top()
+    st.session_state["innerwidth"] = screen_d['innerWidth']
+except:
+    from streamlit_js_eval import streamlit_js_eval
+    streamlit_js_eval(js_expressions='window.innerWidth', key='innerwidth')
+
 
 if "data" not in st.session_state:
     sheet_id = "1t3gF2GR5PydYdEnwuBlwtx9MgpuYCaLk4T4O6PqmFA0"
@@ -24,7 +31,7 @@ search_expander = st.expander("Search", expanded=True)
 def set_show_all(key):
         st.session_state["show_all"] = key
 
-if screen_d['innerWidth'] > 640:
+if st.session_state["innerwidth"] > 640:
     with search_expander:
         season, t_s, t_s_s, text_colon, t_h_s, t_h, m = st.columns([3, 3, 1, 1, 1, 3, 3])
         st.session_state["season"] = season.text_input("Season", key="season_input")
@@ -95,8 +102,8 @@ if screen_d['innerWidth'] > 640:
             # player_s, char_s, escape, decode, hit, rescue, heal, contain, player_h, character_h = st.columns([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
     else:
-        n_cards_per_row = max(1, screen_d["innerWidth"] // 494)
-        st.write(screen_d['innerWidth'])
+        n_cards_per_row = max(1, st.session_state["innerwidth"] // 494)
+        st.write(st.session_state["innerwidth"])
         flag = True
         for n_row, row in data.reset_index().iterrows():
             i = n_row % n_cards_per_row
@@ -120,102 +127,5 @@ if screen_d['innerWidth'] > 640:
                 for j in range(1, 5):
                     sp.markdown(f"<p style='text-align: right; font-weight: bold; color: #92C1C7'>{row['team_s'].strip()}_{row[f'player_s_{j}']}</p>", unsafe_allow_html=True)
                     sc.markdown(f"<p style='text-align: left'>{row[f'character_s_{j}']}</p>", unsafe_allow_html=True)
-
-    # st.table(data)
-else:
-    st.write('''<style>
-            [data-testid="column"] {
-                width: calc(33.3333% - 1rem) !important;
-                flex: 1 1 calc(33.3333% - 1rem) !important;
-                min-width: calc(33% - 1rem) !important;
-            }
-            </style>''', unsafe_allow_html=True)
-    with search_expander:
-        # season, t_s, t_s_s, text_colon, t_h_s, t_h, m = st.columns([3, 3, 1, 1, 1, 3, 3])
-        season, m = st.columns([1, 1])
-        st.session_state["season"] = season.text_input("Season", key="season_input")
-        st.session_state["map"] = m.selectbox("Map", ["", "Arms Factory", "The Red Church", "Sacred Heart Hospital", "Lakeside Village", "Moonlit River Park", "Leo's Memory", "Eversleeping Town", "China Town"])
-        
-        t_s, t_h = st.columns([1, 1])
-
-        st.session_state["team_s"] = t_s.text_input("Survivor Team", key="team_s_input")
-        st.session_state["team_h"] = t_h.text_input("Hunter Team", key="team_h_input")
-        # st.session_state["map"] = m.text_input("Map", value=("" if "map" not in st.session_state else st.session_state["map"]), key="map_input")
-        
-        hp, hc = st.columns([1, 1])
-        st.session_state["player_h"] = hp.text_input("Hunter", key="h_input")
-        st.session_state["character_h"] = hc.text_input("Character", key="hc_input")
-        
-        for i in range(1, 5):
-            sp, sc = st.columns([1, 1])
-            st.session_state[f"player_s_{i}"] = sp.text_input("Survivor", key=f"s{i}_input")
-            st.session_state[f"character_s_{i}"] = sc.text_input("Character", key=f"sc{i}_input")
-
-    for keyword in ["season", "team_h", "team_s", "map", "player_h", "character_h"]:
-        if keyword in st.session_state and st.session_state[keyword] != "":
-            data  = data[data[keyword] == st.session_state[keyword]]
-    for i in range(1, 5):
-        np = f"player_s_{i}" not in st.session_state or st.session_state[f"player_s_{i}"] == ""
-        nc = f"character_s_{i}" not in st.session_state or st.session_state[f"character_s_{i}"] == ""
-        data = data[(((data["player_s_1"] == st.session_state[f"player_s_{i}"]) | np) & ((data["character_s_1"] == st.session_state[f"character_s_{i}"]) | nc)) | 
-                    (((data["player_s_2"] == st.session_state[f"player_s_{i}"]) | np) & ((data["character_s_2"] == st.session_state[f"character_s_{i}"]) | nc)) | 
-                    (((data["player_s_3"] == st.session_state[f"player_s_{i}"]) | np) & ((data["character_s_3"] == st.session_state[f"character_s_{i}"]) | nc)) | 
-                    (((data["player_s_4"] == st.session_state[f"player_s_{i}"]) | np) & ((data["character_s_4"] == st.session_state[f"character_s_{i}"]) | nc))]
-        
-    if not data.equals(st.session_state["data_old"]):
-        set_show_all(None)
-        st.session_state["data_old"] = data
-
-    if "show_all" in st.session_state and st.session_state["show_all"] is not None:
-        data_to_show = data[data["url"] == st.session_state["show_all"]]
-        for n_row, row in data_to_show.reset_index().iterrows():
-            c1, c2, c3 = st.columns([1, 3, 1])
-            c1.caption(f"<p style='text-align: left;'>{row['season'].strip()} W{row['week'].strip()}D{row['day'].strip()}<br>MATCH{row['match'].strip()} ROUND {row['round'].strip()} {row['half'].strip()} HALF</p>", unsafe_allow_html=True)
-            # c2.markdown("# Game Data")
-            c2.markdown(f"<h1 style='text-align: center'>Game Data</h1>", unsafe_allow_html=True)
-            st.write(
-                """
-                <style>
-                div.stButton>button {
-                    float: right;
-                }
-                </style>
-                """,
-                unsafe_allow_html=True,
-            )
-            c3.button("Show less", on_click=set_show_all, args=[None])
-
-            ss, ts, m, th, sh = st.columns([1, 1, 3, 1, 1])
-            ss.markdown(f"<p style='text-align: right; font-weight: bold; font-size: 40px; color: #92C1C7'>{4 - int(row['n_kill'].strip()) + (4 - int(row['n_kill'].strip())) // 4}</p>", unsafe_allow_html=True)
-            ts.markdown(f"<p style='text-align: center; font-weight: bold; font-size: 40px; color: #92C1C7'>{row['team_s'].strip()}</p>", unsafe_allow_html=True)
-            m.markdown(f"<p style='text-align: center; font-size: 40px;'>{row['map'].strip()}</p>", unsafe_allow_html=True)
-            th.markdown(f"<p style='text-align: center; font-weight: bold; font-size: 40px; color: #C87F94'>{row['team_h'].strip()}</p>", unsafe_allow_html=True)
-            sh.markdown(f"<p style='text-align: left; font-weight: bold; font-size: 40px; color: #C87F94'>{int(row['n_kill'].strip()) + (int(row['n_kill'].strip())) // 4}</p>", unsafe_allow_html=True)
-
-            # 여기서부터
-            # player_s, char_s, escape, decode, hit, rescue, heal, contain, player_h, character_h = st.columns([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-
-    else:
-        flag = True
-        for n_row, row in data.reset_index().iterrows():
-            st.write("---")
-        # draw the card
-            # st.caption(f"{row['season'].strip()} W{row['week'].strip()}D{row['day'].strip()}<br>MATCH{row['match'].strip()} ROUND {row['round'].strip()} {row['half'].strip()} HALF", unsafe_allow_html=True)
-            st.markdown(f"<p style='text-align: center; font-size: 14px; color: rgba(49, 51, 63, 0.6)'>{row['season'].strip()} W{row['week'].strip()}D{row['day'].strip()}<br>MATCH{row['match'].strip()} ROUND {row['round'].strip()} {row['half'].strip()} HALF</p>", unsafe_allow_html=True)
-            sp, sc, h, m = st.columns([1, 1, 1, 3])
-            sc.markdown(f"<p style='text-align: center; font-weight: bold; font-size: 20px'>vs</p>", unsafe_allow_html=True)
-            sp.markdown(f"<p style='text-align: right; font-weight: bold; font-size: 20px; color: #92C1C7'>{row['team_s'].strip()} {4 - int(row['n_kill'].strip()) + (4 - int(row['n_kill'].strip())) // 4}</p>", unsafe_allow_html=True)
-            h.markdown(f"<p style='text-align: left; font-weight: bold; font-size: 20px; color: #C87F94'>{int(row['n_kill'].strip()) + int(row['n_kill'].strip()) // 4} {row['team_h'].strip()}</p>", unsafe_allow_html=True)
-            m.markdown(f"<p style='text-align: center; font-weight: bold; font-size: 20px;'>{row['map'].strip()}</p>", unsafe_allow_html=True)
-            p, c = st.columns([1, 1])
-            p.markdown(f"<div style='text-align: right; font-weight: bold; color: #C87F94'>{row['team_h'].strip()}_{row['player_h']}</div>", unsafe_allow_html=True)
-            c.markdown(f"<p style='text-align: left'>{row['character_h']}</p>", unsafe_allow_html=True)
-            for j in range(1, 5):
-                p, c = st.columns([1, 1])
-                p.markdown(f"<p style='text-align: right; font-weight: bold; color: #92C1C7'>{row['team_s'].strip()}_{row[f'player_s_{j}']}</p>", unsafe_allow_html=True)
-                c.markdown(f"<p style='text-align: left'>{row[f'character_s_{j}']}</p>", unsafe_allow_html=True)
-            p, c = st.columns([1, 1])
-            p.markdown(f"<div style='text-align: right'><a href='{row['url'].strip()}' style='text-decoration-line: none; font-size: 30px; color: black'>\u25B6</a></div>", unsafe_allow_html=True)
-            c.button("Show more", key=n_row, on_click=set_show_all, args=[row['url'].strip()])
 
     # st.table(data)
